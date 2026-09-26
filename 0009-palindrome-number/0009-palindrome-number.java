cdd2b343f64c4b7cc98c93aab7d2950eb33eb32e
Gaurav Shingare <gauravshingare14@gmail.com>
2026-09-26 15:48:37 +0530

    Time: 5 ms (84.05%), Space: 45.8 MB (86.57%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        int num=x;
+
+        if(x<0){
+            return false;
+        }
+
+
+        int reverse=0;
+        while(x!=0){
+            int digit=x%10;
+            reverse=reverse*10+ digit;
+
+            x/=10;
+        }
+
+        if(reverse!=num){
+            return false;
+        }
+
+        return true;
+        
+    }
+}
